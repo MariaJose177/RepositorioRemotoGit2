0f1b0d446fd93e054e97f8f6ea65debf44a727cd
Mjromero <Mjromero@192.168.1.151>
2026-01-28 11:24:38 +0100

    Rama1

--- Proyecto2/src/paquete2/Principal.java
+++ Proyecto2/src/paquete2/Principal.java
@@ -4,6 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println ("Estamos haciendo el ejercicio 4.23");
+		System.out.println ("Rama1 - Mensaje1");
+		System.out.println ("Rama2 - Mensaje2");
 	}
 }
 
